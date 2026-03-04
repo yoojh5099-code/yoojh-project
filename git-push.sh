04eb184c34beb182e6a7d43e5bb1943285325b0e
yoojh <you5099@gmail.com>
2026-03-04 11:22:19 +0000

    Update: 2026-03-04 11:22:19

--- git-push.sh
+++ git-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+git remote set-url origin https://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@github.com/yoojh5099-code/yoojh-project.git
 git add .
 git commit -m "${1:-Update: $(date +%Y-%m-%d\ %H:%M:%S)}"
 git push
